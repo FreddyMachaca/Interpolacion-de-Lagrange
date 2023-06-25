ebf92fdc87c96c5bdfa9b72d16290a1156928026
TFreddy <freddyymachaca@gmail.com>
2023-06-25 19:24:49 -0400

    update files

--- script.js
+++ script.js
@@ -1,47 +1,81 @@
-function addInputRow() {
-    let row = document.createElement("div");
-    row.classList.add("input-row");
-    row.innerHTML = `
-        <input class="form-control" placeholder="x">
-        <input class="form-control" placeholder="y">
-    `;
-    document.getElementById("inputPoints").appendChild(row);
-}
+document.addEventListener("DOMContentLoaded", function() {
+    const pointsContainer = document.getElementById("points-container");
+    const addPointBtn = document.getElementById("add-point-btn");
+    const removePointBtn = document.getElementById("remove-point-btn");
+    const calculateBtn = document.getElementById("calculate-btn");
+    let pointIndex = 1;
 
-function lagrange(points, x) {
-    let sum = 0;
-    let n = points.length;
+    addPointBtn.addEventListener("click", function() {
+        const xInputContainer = document.createElement("div");
+        xInputContainer.classList.add("input-container");
+        xInputContainer.innerHTML = `
+            <label for="x-values-${pointIndex}">Coordenadas x de los puntos conocidos:</label>
+            <input type="text" id="x-values-${pointIndex}" placeholder="Ejemplo: 0, 1, 2, 3">
+        `;
+        pointsContainer.appendChild(xInputContainer);
 
-    for (let i = 0; i < n; i++) {
-        let xi = points[i][0], yi = points[i][1];
-        let product = yi;
+        const yInputContainer = document.createElement("div");
+        yInputContainer.classList.add("input-container");
+        yInputContainer.innerHTML = `
+            <label for="y-values-${pointIndex}">Coordenadas y de los puntos conocidos:</label>
+            <input type="text" id="y-values-${pointIndex}" placeholder="Ejemplo: -6, 6, 24, 18">
+        `;
+        pointsContainer.appendChild(yInputContainer);
 
-        for (let j = 0; j < n; j++) {
-            if (i !== j) {
-                let xj = points[j][0];
-                product *= (x - xj) / (xi - xj);
-            }
+        pointIndex++;
+    });
+
+    removePointBtn.addEventListener("click", function() {
+        if (pointIndex > 1) {
+            pointIndex--;
+            const xInputContainer = document.getElementById(`x-values-${pointIndex}`).parentNode;
+            const yInputContainer = document.getElementById(`y-values-${pointIndex}`).parentNode;
+            pointsContainer.removeChild(xInputContainer);
+            pointsContainer.removeChild(yInputContainer);
+        }
+    });
+
+    calculateBtn.addEventListener("click", calculateInterpolation);
+
+    function calculateInterpolation() {
+        const xiValueInput = document.getElementById("xi-value");
+        const resultContainer = document.getElementById("result-container");
+
+        let xValues = [];
+        let yValues = [];
+
+        for (let i = 0; i < pointIndex; i++) {
+            const xValuesInput = document.getElementById(`x-values-${i}`);
+            const yValuesInput = document.getElementById(`y-values-${i}`);
+            xValues = xValues.concat(xValuesInput.value.split(",").map(parseFloat));
+            yValues = yValues.concat(yValuesInput.value.split(",").map(parseFloat));
         }
 
-        sum += product;
+        const xiValue = parseFloat(xiValueInput.value);
+
+        if (xValues.length !== yValues.length) {
+            resultContainer.innerText = "Error: El número de coordenadas x y y debe ser el mismo.";
+            return;
+        }
+
+        const yiValue = lagrangeInterpolate(xValues, yValues, xiValue);
+        resultContainer.innerText = `El valor interpolado de y en x = ${xiValue} es: ${yiValue}`;
     }
 
-    return sum;
-}
+    function lagrangeInterpolate(x, y, xi) {
+        const n = x.length;
+        let yi = 0.0;
 
-function calculate() {
-    let points = [];
-    let inputRows = document.getElementById("inputPoints").getElementsByClassName("input-row");
+        for (let i = 0; i < n; i++) {
+            let term = y[i];
+            for (let j = 0; j < n; j++) {
+                if (j !== i) {
+                    term *= (xi - x[j]) / (x[i] - x[j]);
+                }
+            }
+            yi += term;
+        }
 
-    for (let row of inputRows) {
-        let inputs = row.getElementsByTagName("input");
-        let x = parseFloat(inputs[0].value);
-        let y = parseFloat(inputs[1].value);
-        points.push([x, y]);
+        return yi;
     }
-
-    let x = parseFloat(prompt("Ingrese el valor de x para el que desea interpolar:"));
-    let result = lagrange(points, x);
-
-    document.getElementById("result").textContent = "El resultado aproxiamado es: " + result;
-}
+});
